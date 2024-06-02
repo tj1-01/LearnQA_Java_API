4a9b85197bd0545e69777c49d5bab9c627a8d062
Iuliiа Liubomirova <tj1-01@yandex.ru>
2024-06-02 19:18:29 +0300

    Lesson1 Hello from

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+
+    @Test
+    public void testHelloWorldFrom(){
+        System.out.println("Hello from Iuliia");
+    }
+}
