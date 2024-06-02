a482cae165fd630ffd1f8272f36a59597c80bc70
Iuliiа Liubomirova <tj1-01@yandex.ru>
2024-06-02 19:12:38 +0300

    Lesson1 get_text

---
+++ src/test/java/LessonOneGetText.java
@@ -0,0 +1,17 @@
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import org.junit.jupiter.api.Test;
+
+public class LessonOneGetText {
+
+
+    @Test
+    public void testGetText(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
