0b35d293340aa2be840e93267aec98d40249417b
Iuliiа Liubomirova <tj1-01@yandex.ru>
2024-06-08 13:36:35 +0300

    Ex6: Редирект

---
+++ src/test/java/redirectHmwrk.java
@@ -0,0 +1,28 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class redirectHmwrk {
+
+    /*
+    Необходимо написать тест,
+    который создает GET-запрос на адрес: https://playground.learnqa.ru/api/long_redirect
+
+    С этого адреса должен происходит редирект на другой адрес.
+    Наша задача — распечатать адрес, на который редиректит указанные URL.
+     */
+
+    @Test
+    public void redirectHmwrk(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+}
