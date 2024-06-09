7c9415149d0c4d89b19a294b4dc618cdf80177c4
Iuliiа Liubomirova <tj1-01@yandex.ru>
2024-06-09 16:14:52 +0300

    fix cookie

--- src/test/java/findPassEx9.java
+++ src/test/java/findPassEx9.java
@@ -19,8 +19,7 @@ public class findPassEx9 {
         String test_pass="super_admin";
 
         List<String> testPass = new ArrayList<>();
-        //testPass = fullTestPassList();
-        testPass = fullTestPassListText();
+        testPass = fullTestPassList();
 
         if (testPass.isEmpty()) {System.out.println("Списка паролей нет, тест прекращен"); return;}
 
@@ -45,7 +44,8 @@ public class findPassEx9 {
             Response responseA = RestAssured
                     .given()
                     .body(data)
-                    .cookie(responseCookie)
+                    .cookie("auth_cookie",responseCookie)
+                    .when()
                     .post(urlCheckAuth)
                     .andReturn();
 
@@ -53,93 +53,15 @@ public class findPassEx9 {
                 System.out.println("Корректное сочетание: " + data);
                 responseA.print();
                 return;
-            } else {
-                //System.out.println("Некорректные значения: " + data);
             }
         }
 
-    }
-
-    public List<String> fullTestPassListText(){
-        List<String> testPasws = new ArrayList<>();
-        testPasws.add("password");
-        testPasws.add("123456");
-        testPasws.add("12345678");
-        testPasws.add("qwerty");
-        testPasws.add("abc123");
-        testPasws.add("monkey");
-        testPasws.add("1234567");
-        testPasws.add("letmein");
-        testPasws.add("trustno1");
-        testPasws.add("dragon");
-        testPasws.add("baseball");
-        testPasws.add("111111");
-        testPasws.add("iloveyou");
-        testPasws.add("master");
-        testPasws.add("sunshine");
-        testPasws.add("ashley");
-        testPasws.add("bailey");
-        testPasws.add("passw0rd");
-        testPasws.add("shadow");
-        testPasws.add("123123");
-        testPasws.add("654321");
-        testPasws.add("superman");
-        testPasws.add("qazwsx");
-        testPasws.add("michael");
-        testPasws.add("Football");
-        testPasws.add("password");
-        testPasws.add("123456789");
-        testPasws.add("12345");
-        testPasws.add("princess");
-        testPasws.add("admin");
-        testPasws.add("welcome");
-        testPasws.add("666666");
-        testPasws.add("!@#$%^&*");
-        testPasws.add("charlie");
-        testPasws.add("aa123456");
-        testPasws.add("donald");
-        testPasws.add("password1");
-        testPasws.add("qwerty123");
-        testPasws.add("1q2w3e4r");
-        testPasws.add("qwertyuiop");
-        testPasws.add("555555");
-        testPasws.add("lovely");
-        testPasws.add("7777777");
-        testPasws.add("888888");
-        testPasws.add("123qwe");
-        testPasws.add("jesus");
-        testPasws.add("ninja");
-        testPasws.add("mustang");
-        testPasws.add("adobe123");
-        testPasws.add("1234567890");
-        testPasws.add("photoshop");
-        testPasws.add("1234");
-        testPasws.add("azerty");
-        testPasws.add("0000000");
-        testPasws.add("access");
-        testPasws.add("696969");
-        testPasws.add("batman");
-        testPasws.add("1qaz2wsx");
-        testPasws.add("login");
-        testPasws.add("solo");
-        testPasws.add("starwars");
-        testPasws.add("121212");
-        testPasws.add("flower");
-        testPasws.add("hottie");
-        testPasws.add("loveme");
-        testPasws.add("zaq1zaq1");
-        testPasws.add("hello");
-        testPasws.add("freedom");
-        testPasws.add("whatever");
-        return testPasws;
     }
 
     public List<String> fullTestPassList(){
 
         List<String> testPasws = new ArrayList<>();
 
-        String text1 = "Top 25 most common passwords by year according to SplashData";
-        String text2 = "</table>";
         String tag1="<td align=\"left\">";
         String tag2 ="</td>";
 
@@ -160,11 +82,10 @@ public class findPassEx9 {
                 text = text.substring(s2);
             }
         }
+
         Set<String> setUnicPswrd = new LinkedHashSet<>(testPasws);
         testPasws = new ArrayList<>(setUnicPswrd);
 
-        System.out.println(testPasws);
         return testPasws;
-
     }
 }
