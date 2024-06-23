53fd89be9f8fb88abbca223b163dd69c25318260
Iuliiа Liubomirova <tj1-01@yandex.ru>
2024-06-23 12:29:38 +0300

    Ex12: Header test

---
+++ src/test/java/headerTestEx12.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class headerTestEx12 {
+
+    @Test
+    public void headertest(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        assertEquals("Some secret value",response.getHeader("x-secret-homework-header"),"header определен не верно");
+    }
+}
