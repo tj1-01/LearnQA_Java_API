70691864f5d7250b3151935c961d8a7fdaccadaf
Iuliiа Liubomirova <tj1-01@yandex.ru>
2024-06-23 12:20:43 +0300

    Ex11: find cookie and test it

---
+++ src/test/java/cookieTestEx11.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class cookieTestEx11 {
+
+    @Test
+    public void findCokkie(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        assertEquals("I find cookie", response.cookies(),"Тест падает, но показывает куки:");
+    }
+
+    @Test
+    public void cookieTestEx11(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        //проверка куки и значения в ней
+        assertEquals("hw_value", response.getCookie("HomeWork"),"Неожиданный набор Cookie и значения");
+    }
+}
